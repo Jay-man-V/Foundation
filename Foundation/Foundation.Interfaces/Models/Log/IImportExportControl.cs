705b312d3b03e5d67e95688116f2cf82a3bfc9b4
Jayesh Varsani <jayesh.varsani@datalore.me.uk>
2025-07-06 20:17:28 +0100

    .

---
+++ Foundation/Foundation.Interfaces/Models/Log/IImportExportControl.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IImportExportControl.cs" company="JDV Software Ltd">
+//     Copyright (c) JDV Software Ltd. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Foundation.Interfaces
+{
+    /// <summary>
+    /// The Import Export Control model interface
+    /// </summary>
+    public interface IImportExportControl : IFoundationModel
+    {
+        /// <summary>The Date/time the Import or Export process was last run.</summary>
+        /// <value>The processed on.</value>
+        DateTime ProcessedOn { get; set; }
+
+        /// <summary>Gets or sets the name.</summary>
+        /// <value>The name.</value>
+        String Name { get; set; }
+    }
+}
---
+++ Foundation/Foundation.Interfaces/Repository/Log/IEventLogRepository.cs
@@ -0,0 +1,39 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IEventLogRepository.cs" company="JDV Software Ltd">
+//     Copyright (c) JDV Software Ltd. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+
+namespace Foundation.Interfaces
+{
+    /// <summary>
+    /// The Event Log Data Access interface
+    /// </summary>
+    public interface IEventLogRepository : IFoundationModelRepository<IEventLog>
+    {
+        /// <summary>
+        /// Gets the specified event log.
+        /// </summary>
+        /// <param name="logId">The event log identifier.</param>
+        /// <returns>Loaded event log</returns>
+        IEventLog Get(LogId logId);
+
+        /// <summary>
+        /// Retrieves the latest <seealso cref="IEventLog"/> entry for the given parameters
+        /// regardless of completion status
+        /// <para>
+        /// <paramref name="isFinished"/> must be specified.
+        /// The remaining parameters can be specified or be left as default
+        /// </para>
+        /// </summary>
+        /// <param name="isFinished"></param>
+        /// <param name="scheduledTaskId"></param>
+        /// <param name="batchName"></param>
+        /// <param name="processName"></param>
+        /// <param name="taskName"></param>
+        /// <returns></returns>
+        IEventLog GetLatest(Boolean isFinished, EntityId scheduledTaskId = new(), String? batchName = null, String? processName = null, String? taskName = null);
+    }
+}
