d550793cdc1b976bd4e24b2df38ab87dcbd34dad
Jayesh Varsani <jayesh.varsani@datalore.me.uk>
2026-02-14 22:53:40 +0000

    .

---
+++ Applications/JDV Accounts/_Tests/JDV.Accounts.Tests.Views/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace JDV.Accounts.Tests.Views
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
