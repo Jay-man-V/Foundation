39b65e8cbf28e5388ab5194d26f062be2faf26ed
Jayesh Varsani <jayesh.varsani@datalore.me.uk>
2026-08-31 20:29:58 +0100

    Solution Nugets updates

---
+++ Foundation/Foundation.Repository/DataProvider/SecretDataProvider.cs
@@ -0,0 +1,39 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="SecretDataProvider.cs" company="JDV Software Ltd">
+//     Copyright (c) JDV Software Ltd. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using Foundation.Common;
+using Foundation.Interfaces;
+
+namespace Foundation.Repository.DataProvider
+{
+    /// <summary>
+    /// Defines the Secret Data Provider class
+    /// </summary>
+    /// <see cref="ISecretDataProvider" />
+    /// <seealso cref="DataProvider" />
+    [DependencyInjectionTransient]
+    public class SecretDataProvider : DataProvider, ISecretDataProvider
+    {
+        public SecretDataProvider
+        (
+            ICore core,
+            ISystemConfigurationService systemConfigurationService
+        ) :
+            base
+            (
+                core,
+                systemConfigurationService,
+                "Secret"
+            )
+        {
+            LoggingHelpers.TraceCallEnter();
+
+            // Does nothing
+
+            LoggingHelpers.TraceCallReturn();
+        }
+    }
+}
